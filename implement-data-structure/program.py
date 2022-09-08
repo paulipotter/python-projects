49e2c1ba923779826b7377f494adc65612466b4e
Paula Mendez <dev.paulamendez@gmail.com>
2022-09-08 14:25:01 -0500

    program

--- implement-data-structure/program.py
+++ implement-data-structure/program.py
@@ -7,7 +7,6 @@ def q_round(source: Q, dest: Q, original_q: str):
     2. Take the next off the top and put it on the bottom of the deck in your hand.
     3. Continue steps 1 and 2 until all cards are on the table. This is a round.
     4. Pick up the deck from the table and repeat steps 1-3 until the deck is in original order.
-
     :param source: the original deck with N cards
     :param dest: the empty deck
     :param original_q: a string that holds the original order to later compare to the deck
@@ -48,8 +47,8 @@ def q_round(source: Q, dest: Q, original_q: str):
         print("original", original_q)
         print("deck    ", deck)
         print("table   ", table)
-    print("-----------")
     print("Total Number of rounds:", number_of_rounds)
+    print("-----------")
 
 
 def get_user_input() -> int:
@@ -59,7 +58,7 @@ def get_user_input() -> int:
     :return: the valid N value to be used to generate the deck
     """
     while True:
-        num = input("Please enter a number: ")
+        num = input("Please enter a number: ").strip()
         try:
             val = int(num)
             print("Input is an integer number.")
